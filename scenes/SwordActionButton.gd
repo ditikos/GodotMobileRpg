f249b04613efa32b8437ecba5f71ac3f0dcbe1a4
Panagiotis Chalatsakos <chibioni@gmail.com>
2019-08-20 20:59:59 +0300

    Got Minotaur working back

--- scenes/SwordActionButton.gd
+++ scenes/SwordActionButton.gd
@@ -12,10 +12,17 @@ func _on_pressed() -> void:
 	var playerStats = BattleUnits.PlayerStats
 	
 	if enemy != null and playerStats != null:
+		# Slash animation
+		# Sound is triggered in animation
 		create_slash(enemy.global_position)
+		
+		# change this when you get xp working 
+		# change this when you get inventory working
 		# change 1: dynamic damage instead of 4.
-		var damage = clamp(rng.randi_range(0, 4), 0, 4)
-		enemy.take_damage(damage)
+		var damage = clamp(rng.randi_range(-1, 3), -1, 3)
+		enemy.take_damage(2 + damage)
+		
+		# Get stats updates
 		playerStats.ap -= 1
 		playerStats.mp += 2
 
